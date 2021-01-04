869dc8ae8b69707c3923995af6a19594b27247f7
dsingal2 <dhairyasingal08@gmail.com>
2021-01-03 17:08:43 -0800

    dino cant fall below the floor and cant jump twice

--- gui.py
+++ gui.py
@@ -17,10 +17,13 @@ basePos = 0
 
 dino = pygame.image.load('images/dino.png').convert()
 dino = pygame.transform.scale(dino, (109, 80))
-dino_rect = dino.get_rect(center = (50, 270))
+dino_rect = dino.get_rect(center = (100, 270))
 
 dino_movement = 0
 gravity = 0.5
+
+# cactus = pygame.image.load('images/cactus.png').convert()
+# # cactus = pygame.transform.scale(cactus, (109, 80))
 
 def baseAnimation():
     screen.blit(baseSurface, (basePos,300))
@@ -35,14 +38,15 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                dino_movement = 0
-                dino_movement -= 10
+                if dino_rect.centery == 270:
+                    dino_movement = 0
+                    dino_movement -= 10
 
     
     screen.blit(backgroundSurface, (0,0))
-
-    dino_movement += gravity
-    dino_rect.centery += dino_movement
+    # screen.blit(cactus, (0,0))
+    dino_movement = min((dino_movement + gravity),10)
+    dino_rect.centery = min ((dino_rect.centery + dino_movement), 270)
     screen.blit(dino, dino_rect)
 
     basePos-=1
