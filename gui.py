25870a2f3fbcf6efd77eb24867bdc0775ade40f8
dsingal2 <dhairyasingal08@gmail.com>
2021-01-06 18:23:55 -0800

    added collision detection between  pipe and dino

--- gui.py
+++ gui.py
@@ -8,6 +8,8 @@ pygame.init()
 screen = pygame.display.set_mode((820,332))
 clock = pygame.time.Clock()
 
+game_active = True
+
 backgroundSurface = pygame.image.load('images/bg.png').convert()
 backgroundSurface = pygame.transform.scale(backgroundSurface, (820,332))
 
@@ -17,14 +19,14 @@ baseSurface = pygame.transform.scale(baseSurface, (820, 100))
 basePos = 0
 
 dino = pygame.image.load('images/dino.png').convert()
-dino = pygame.transform.scale(dino, (109, 80))
-dino_rect = dino.get_rect(center = (100, 270))
+dino = pygame.transform.scale(dino, (130, 96))
+dino_rect = dino.get_rect(center = (130, 270))
 
 dino_movement = 0
 gravity = 0.5
 
-cactus = pygame.image.load('images/cactus.png')
-cactus = pygame.transform.scale(cactus, (125, 125))
+cactus = pygame.image.load('images/pipe-red.png').convert()
+cactus = pygame.transform.scale(cactus, (21, 128))
 cactus_list = []
 
 SPAWNCACTUS = pygame.USEREVENT
@@ -35,8 +37,8 @@ def baseAnimation():
     screen.blit(baseSurface, (basePos + 820 ,275))
 
 def create_cactus():
-    new_cactus = cactus.get_rect(midtop = (random.choice([820,950]),165))
-    sec_cactus = cactus.get_rect(midtop = (1000 - random.choice([100,150]), 165))
+    new_cactus = cactus.get_rect(center = (random.choice([820,950]),270))
+    sec_cactus = cactus.get_rect(center = (random.choice([820,950]), 270))
     return new_cactus, sec_cactus
 
 def move_cactus(cactus_list):
@@ -49,6 +51,13 @@ def move_cactus(cactus_list):
 def draw_cactus(cactus_list):
     for c in cactus_list:
         screen.blit(cactus, c)
+
+def check_collision(cactus_list):
+    for c in cactus_list:
+        if dino_rect.colliderect(c):
+            return False
+    return True
+
        
     
 while True:
@@ -58,7 +67,7 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                if dino_rect.centery == 245:
+                if dino_rect.centery == 240:
                     dino_movement = 0
                     dino_movement -= 12.5
         if event.type == SPAWNCACTUS:
@@ -71,14 +80,16 @@ while True:
     screen.blit(backgroundSurface, (0,0))
     # screen.blit(cactus, (100,165))
 
-    # Dino
-    dino_movement = min((dino_movement + gravity),10)
-    dino_rect.centery = min ((dino_rect.centery + dino_movement), 245)
-    screen.blit(dino, dino_rect)
+    if game_active:
+        # Dino
+        dino_movement = min((dino_movement + gravity),10)
+        dino_rect.centery = min ((dino_rect.centery + dino_movement), 240)
+        screen.blit(dino, dino_rect)
 
-    # Cactus
-    cactus_list = move_cactus(cactus_list)
-    draw_cactus(cactus_list)
+        game_active = check_collision(cactus_list)
+        # Cactus
+        cactus_list = move_cactus(cactus_list)
+        draw_cactus(cactus_list)
 
     basePos-=1
     if basePos < -820:
